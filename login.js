b8d9762246df10d1c5345f69d8b93877b83531d5
sworupkarki <sworupkarki4@gmail.com>
2023-07-19 07:53:33 +0545

    first commit

---
+++ login.js
@@ -0,0 +1,54 @@
+const mysql = require("mysql");
+const express = require("express");
+const bodyParser = require("body-parser");
+const encoder = bodyParser.urlencoded();
+
+
+const app = express();
+app.use("/assets",express.static("assets"));
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "kamala@5420",
+    database: "loginjs"
+});
+
+// connect to the database
+connection.connect(function(error){
+    if (error) throw error
+    else console.log("connected to the database successfully!")
+});
+
+
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/",encoder, function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+
+    connection.query("select * from loginuser where user_name = ? and password = ?",[username,password],function(error,results,fields){
+        if (results.length > 0) {
+            res.redirect("/welcome");
+        } else {
+            res.redirect("/");
+        }
+        res.end();
+    })
+})
+
+
+
+
+// when login is success
+app.get("/welcome",function(req,res){
+    res.sendFile(__dirname + "/welcome.html")
+})
+
+
+// set app port 
+app.listen(4000);
